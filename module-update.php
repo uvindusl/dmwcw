8e3c0b80b99b028d68a7b7c63e1438d7486f8f81
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2024-11-15 23:30:40 +0530

    faced error

---
+++ module-update.php
@@ -0,0 +1,30 @@
+<?php
+$sname = "localhost";
+$uname = "root";
+$pass = "";
+$dbname = "dmw_cw";
+
+$conn = mysqli_connect($sname, $uname, $pass, $dbname, 3307);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $module_id = $_POST['module-id'];
+    $module_name = $_POST['module-name'];
+    $description = $_POST['description'];
+
+    $sql = "UPDATE module SET MDiscription='$description', MName='$module_name' WHERE MID='$module_id'";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: viewmodule.php");
+        echo "<p style='color:green;'>Module updated successfully!</p>";
+    } else {
+        echo "<p style='color:red;'>Error: " . mysqli_error($conn) . "</p>";
+    }
+}
+
+mysqli_close($conn);
+?>
